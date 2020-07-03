50559e225dcb220d3d081fcf74dc747b246b9055
Akutziki <aku.lappalainen@edu.turkuamk.fi>
2020-07-03 12:56:00 +0300

    Added DataEntry.js

--- src/Components/DataEntry.js
+++ src/Components/DataEntry.js
@@ -8,8 +8,7 @@ function DataEntry(props) {
         <>
             {showData ? (
                 <>
-                    <div onClick={() => { dataArray.splice(props.index, 1); setShowData(false) }} className="text-center bg-blue-500 text-white active:bg-blue-600 font-bold uppercase text-sm px-6 py-3 mt-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 "
-                    >
+                    <div onClick={() => { dataArray.splice(props.index, 1); setShowData(false) }} className="text-center bg-blue-500 text-white active:bg-blue-600 font-bold uppercase text-sm px-6 py-3 mt-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ">
                         <p>Määrä:{props.data.amount}</p>
                         <p>Syy:{props.data.reason}</p>
                         <p>Tulo/meno:{props.data.type}</p>
